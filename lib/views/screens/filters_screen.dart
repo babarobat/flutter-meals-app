54696ccb41a371415da50cf7311aeb057de8b46c
Denis Kotenko <d.kotenko@rj-games.com>
2021-11-28 15:38:26 +0300

    filters screen

--- lib/views/screens/filters_screen.dart
+++ lib/views/screens/filters_screen.dart
@@ -9,6 +9,11 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  var isGlutenFree = false;
+  var isVegan = false;
+  var isVegetarian = false;
+  var isLactoseFree = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +21,77 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Filters'),
       ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _glutenFilter(),
+                _veganFilter(),
+                _vegetarianFilter(),
+                _lactoseFreeFilter(),
+              ],
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  Widget _filterEntry(String title, String description, bool current,
+      Function(bool) onChangedCallback) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      value: current,
+      onChanged: onChangedCallback,
+    );
+  }
+
+  Widget _glutenFilter() {
+    return _filterEntry(
+        'gluten free',
+        'no gluten ',
+        isGlutenFree,
+        (value) => setState(() {
+              isGlutenFree = value;
+            }));
+  }
+
+  Widget _veganFilter() {
+    return _filterEntry(
+        'vegan',
+        'vegan ',
+        isVegan,
+        (value) => setState(() {
+              isVegan = value;
+            }));
+  }
+
+  Widget _vegetarianFilter() {
+    return _filterEntry(
+        'vegetarian',
+        'vegetarian ',
+        isVegetarian,
+        (value) => setState(() {
+              isVegetarian = value;
+            }));
+  }
+
+  Widget _lactoseFreeFilter() {
+    return _filterEntry(
+        'lactose free',
+        'lactose free ',
+        isLactoseFree,
+        (value) => setState(() {
+              isLactoseFree = value;
+            }));
   }
 }
